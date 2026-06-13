c49bfe7977fb8a2273386a945f9fae4da126da1b
ritesh kumar <riteshyadav8995@gmail.com>
2026-06-13 14:45:54 +0530

    added the backend base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api" || ""
+  baseURL: "http://localhost:5000/api" || "https://tanvi-boutique.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
